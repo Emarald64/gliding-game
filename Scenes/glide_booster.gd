33dec15e90daa8dbc3da0fbe39c34efc83c243ba
Alex Giller <alexgiller@proton.me>
2025-11-27 12:40:38 -0500

    changed wind zone to use gpu particles

--- Scenes/glide_booster.gd
+++ Scenes/glide_booster.gd
@@ -2,16 +2,21 @@ class_name GlideBooster extends Area2D
 
 @export var boost:=1.5
 
+const ParticleXSize=2
+const ParticleYSizeFactor=10
+const ParticleSpeedFactor=500
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$CPUParticles2D.position.y=$CollisionShape2D.shape.size.y/2
-	$CPUParticles2D.emission_rect_extents.x=$CollisionShape2D.shape.size.x/2
-	$CPUParticles2D.initial_velocity_min=boost*500
-	$CPUParticles2D.lifetime=$CollisionShape2D.shape.size.y/(boost*500)
+	$GPUParticles2D.process_material.emission_shape_offset.y=$CollisionShape2D.shape.size.y/2
+	$GPUParticles2D.process_material.emission_box_extents.x=$CollisionShape2D.shape.size.x/2
+	$GPUParticles2D.process_material.initial_velocity_min=boost*ParticleSpeedFactor
+	$GPUParticles2D.lifetime=$CollisionShape2D.shape.size.y/(boost*ParticleSpeedFactor)
 	
-	$CPUParticles2D.scale_amount_min=boost*10
-	$CPUParticles2D.scale_curve_x.set_point_value(0,0.2/boost)
-
+	$GPUParticles2D.process_material.scale_min=boost*ParticleYSizeFactor
+	$GPUParticles2D.process_material.scale_curve.curve_x.set_point_value(0,ParticleXSize/boost/ParticleYSizeFactor)
+	
+	$GPUParticles2D.visibility_rect=Rect2(-$CollisionShape2D.shape.size/2,$CollisionShape2D.shape.size)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
